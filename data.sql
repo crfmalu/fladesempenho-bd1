52eb40805ed2466686e9f79e4e47a8a44a309b37
Maria Luiza Pontes <mlpontes.510@gmail.com>
2026-06-24 22:59:00 -0300

    Melhorando script de inserção de dados e criando script de consulta

--- data.sql
+++ data.sql
@@ -1,62 +1,124 @@
 INSERT INTO competicao (nome_camp, ano_realizacao, regulamento)
-VALUES ('Campeonato Brasileiro', 2026, 'Pontos Corridos');
+VALUES
+    ('Campeonato Carioca', 2026, 'Mata-Mata'),
+    ('Campeonato Brasileiro', 2026, 'Pontos Corridos');
 
 INSERT INTO adversario (local_origem, nome_clube)
-VALUES ('São Paulo', 'São Paulo');
+VALUES 
+    ('Rio de Janeiro', 'Madureira'),
+    ('Rio de Janeiro', 'Fluminense'),
+    ('Minas Gerais', 'Cruzeiro'),
+    ('Rio de Janeiro', 'Botafogo');
 
 INSERT INTO tecnico (data_inicio, esquema_tatico, nome)
-VALUES ('2024-09-30', '4-2-3-1', 'Filipe Luís');
+VALUES 
+    ('2024-09-30', '4-2-3-1', 'Filipe Luís'),
+    ('2024-04-03', '4-4-2', 'Leonardo Jardim');
 
 INSERT INTO atleta (posicao, nome, status_elenco, numero_camisa)
 VALUES 
     ('ATACANTE', 'PEDRO', 'TITULAR', 9),
     ('ATACANTE', 'EVERTON', 'TITULAR', 11),
-    ('ATACANTE', 'GONZALO PLATA', 'TITULAR', 19),
-    ('MEIO-CAMPO', 'JORGE CARRASCAL', 'TITULAR', 15),
-    ('MEIO-CAMPO', 'ERICK PULGAR', 'TITULAR', 5),
+    ('ATACANTE', 'JORGE CARRASCAL', 'TITULAR', 15),
+    ('MEIO-CAMPO', 'LUCAS PAQUETÁ', 'TITULAR', 20),
+    ('MEIO-CAMPO', 'NICO DE LA CRUZ', 'TITULAR', 18),
     ('MEIO-CAMPO', 'EVERTTON ARAÚJO', 'TITULAR', 52),
     ('LATERAL', 'ALEX SANDRO', 'TITULAR', 26),
-    ('ZAGUEIRO', 'LEO PEREIRA', 'TITULAR', 4),
+    ('ZAGUEIRO', 'VITÃO', 'TITULAR', 44),
     ('ZAGUEIRO', 'LEO ORTIZ', 'TITULAR', 3),
-    ('LATERAL', 'VARELA', 'TITULAR', 2),
-    ('GOLEIRO', 'AGUSTIN ROSSI', 'TITULAR', 1);
+    ('LATERAL', 'EMERSON ROYAL', 'TITULAR', 22),
+    ('GOLEIRO', 'ANDREW VENTURA', 'TITULAR', 42),
+    ('ATACANTE', 'SAMUEL LINO', 'RESERVA', 16),
+    ('ATACANTE', 'LUIZ ARAÚJO', 'RESERVA', 7),
+    ('ATACANTE', 'WALLACE YAN', 'RESERVA', 64),
+    ('ATACANTE', 'GONZALO PLATA', 'RESERVA', 19),
+    ('MEIO-CAMPO', 'ERICK PULGAR', 'RESERVA', 5),
+    ('MEIO-CAMPO', 'ARRASCAETA', 'RESERVA', 10),
+    ('MEIO-CAMPO', 'JORGINHO', 'RESERVA', 21),
+    ('LATERAL', 'VARELA', 'RESERVA', 2),
+    ('LATERAL', 'AYRTON LUCAS', 'RESERVA', 6),
+    ('ZAGUEIRO', 'LEO PEREIRA', 'RESERVA', 4),
+    ('GOLEIRO', 'ANDREW VENTURA', 'TITULAR', 42);
 
 INSERT INTO partidas (mando_campo, gols_pro, gols_contra, data_hora, c_id_competicao, a_id_adversario, t_id_tecnico)
-VALUES ('FORA', 1, 2, '2026-01-28 21:30:00', 1, 1, 1 );
+VALUES 
+    ('FORA', 0, 8, '2026-03-02 21:00:00', 1, 1, 1 ),
+    ('FORA', 0, 0, '2026-03-08 18:00:00', 1, 2, 2 ),
+    ('CASA', 0, 2, '2026-03-11 21:30:00', 2, 3, 2 ),
+    ('FORA', 0, 3, '2026-03-14 20:30:00', 2, 4, 2 );
 
 INSERT INTO participacao (gols_marcados, cartao_am, cartao_verm, minutos, assistencias, id_atleta, id_partida)
 VALUES
-    (0, 0, 0, 56, 1, 1, 1),
-    (0, 0, 0, 62, 0, 2, 1),
-    (1, 0, 0, 90, 0, 3, 1),
-    (0, 0, 0, 62, 0, 4, 1),
-    (0, 0, 0, 90, 0, 5, 1),
-    (0, 0, 0, 56, 0, 6, 1),
-    (0, 0, 0, 90, 0, 7, 1),
-    (0, 0, 0, 90, 0, 8, 1),
-    (0, 0, 0, 90, 0, 9, 1),
-    (0, 0, 0, 90, 0, 10, 1),
-    (0, 0, 0, 90, 0, 11, 1);
+    (4, 0, 0, 90, 1, 1, 1),    
+    (0, 0, 0, 73, 0, 1, 2),
+    (1, 0, 0, 89, 0, 1, 3),
+    (1, 0, 0, 90, 0, 1, 4),
+    (0, 0, 0, 68, 0, 2, 1),    
+    (0, 0, 0, 26, 0, 2, 2),
+    (0, 0, 0, 65, 0, 2, 3),
+    (0, 0, 0, 57, 2, 3, 1),    
+    (0, 0, 0, 64, 0, 3, 2),
+    (1, 0, 0, 18, 0, 3, 3),
+    (0, 0, 0, 86, 0, 3, 4),
+    (2, 0, 0, 90, 0, 4, 1),    
+    (0, 0, 0, 26, 0, 4, 2),
+    (0, 0, 0, 74, 0, 4, 3),
+    (0, 0, 0, 90, 0, 4, 4),
+    (0, 0, 0, 57, 0, 5, 1),    
+    (0, 1, 0, 85, 1, 6, 1),    
+    (0, 0, 0, 20, 0, 6, 4),
+    (0, 0, 0, 90, 1, 7, 1),    
+    (0, 0, 0, 90, 0, 7, 2),
+    (0, 0, 0, 90, 0, 7, 3),
+    (0, 0, 0, 65, 0, 7, 4),
+    (0, 0, 0, 90, 0, 8, 1),    
+    (0, 0, 0, 25, 0, 8, 4),
+    (0, 0, 0, 85, 0, 9, 1),    
+    (0, 0, 0, 90, 0, 9, 2),
+    (0, 0, 0, 90, 0, 9, 3),
+    (0, 0, 0, 90, 0, 9, 4),
+    (0, 0, 0, 90, 1, 10, 1),   
+    (0, 0, 0, 74, 0, 10, 3),
+    (0, 0, 0, 90, 0, 11, 1),   
+    (1, 0, 0, 22, 0, 12, 1),   
+    (0, 1, 0, 64, 0, 12, 2),
+    (0, 0, 0, 25, 1, 12, 3),
+    (1, 0, 0, 90, 0, 12, 4),
+    (0, 0, 0, 33, 0, 13, 1),   
+    (0, 0, 0, 5, 0, 13, 2),
+    (0, 0, 0, 1, 0, 13, 3),
+    (0, 0, 0, 4, 0, 13, 4),                
+    (0, 0, 0, 5, 0, 14, 1),   
+    (0, 0, 0, 1, 0, 14, 3),  
+    (0, 0, 0, 17, 0, 15, 2),   
+    (0, 0, 0, 4, 0, 15, 4),   
+    (0, 0, 0, 90, 0, 16, 2),   
+    (0, 1, 0, 90, 0, 16, 3),
+    (0, 1, 0, 70, 0, 16, 4),  
+    (0, 0, 0, 85, 0, 17, 2),   
+    (0, 0, 0, 89, 0, 17, 3),  
+    (0, 1, 0, 90, 0, 18, 2),   
+    (0, 0, 0, 90, 0, 18, 3),
+    (0, 1, 0, 86, 0, 18, 4),    
+    (0, 0, 0, 90, 0, 19, 2),   
+    (0, 0, 0, 16, 0, 19, 3),
+    (0, 0, 0, 90, 2, 19, 4),  
+    (0, 0, 0, 25, 0, 20, 4),       
+    (0, 0, 0, 90, 0, 21, 2),   
+    (0, 1, 0, 90, 0, 21, 3),
+    (1, 0, 0, 65, 0, 21, 4),
+    (0, 0, 0, 90, 0, 22, 2),   
+    (0, 0, 0, 90, 0, 22, 3),
+    (0, 0, 0, 90, 0, 22, 4);             
+
+
 
 START TRANSACTION; 
 
 UPDATE atleta
 SET status_elenco = 'RESERVA'
-WHERE id_atleta IN (1, 2, 4, 6);
-
-INSERT INTO atleta (posicao, nome, status_elenco, numero_camisa)
-VALUES
-    ('ATACANTE', 'BRUNO HENRIQUE', 'RESERVA', 27),
-    ('ATACANTE', 'SAMUEL LINO', 'RESERVA', 16),
-    ('MEIO-CAMPO', 'ARRASCAETA', 'RESERVA', 10),
-    ('MEIO-CAMPO', 'JORGINHO', 'RESERVA', 21);
-
-INSERT INTO participacao (gols_marcados, cartao_am, cartao_verm, minutos, assistencias, id_atleta, id_partida)
-VALUES
-    (0, 0, 0, 28, 0, 12, 1),
-    (0, 0, 0, 28, 0, 13, 1),
-    (0, 0, 0, 34, 0, 14, 1),
-    (0, 0, 1, 34, 0, 15, 1);
+WHERE id_atleta IN (1, 12, 17, 3),
+WHERE id_partida IN = 2
 
 COMMIT; 
 
--- queries.sql
+++ queries.sql
@@ -1,4 +1,4 @@
-# Ver a data do jogo, se jogamos em casa ou fora, os gols e o nome do adversário
+# data do jogo, se jogamos em casa ou fora, os gols e o nome do adversário
 SELECT 
     partidas.data_hora AS 'Data do Jogo',
     partidas.mando_campo AS 'Mando de Campo',
@@ -8,7 +8,7 @@ SELECT
 FROM partidas
 JOIN adversario ON partidas.a_id_adversario = adversario.id_adversario;
 
-# Queremos listar o nome do jogador que fez o gol, a posição dele e quantos minutos ele jogou naquela partida
+# nome do jogador que fez o gol, a posição dele e quantos minutos ele jogou naquela partida
 SELECT 
     atleta.nome AS 'Jogador',
     atleta.posicao AS 'Posição',
@@ -17,12 +17,12 @@ FROM participacao
 JOIN atleta ON participacao.id_atleta = atleta.id_atleta
 WHERE participacao.id_partida = 3;
 
-# Encontre o nome e o esquema tático do técnico
+# nome e o esquema tático do técnico a partir de 2024
 SELECT
     tecnico.nome AS 'Técnico do Time',
     tecnico.esquema_tatico AS 'Esquema Tático'
 FROM tecnico
-WHERE tecnico.data_inicio >= 2024
+WHERE tecnico.data_inicio >= '2024-01-01';
 
 # Nome e Número da camisa de todos os jogadores que tem posição de goleiro
 SELECT 
@@ -31,13 +31,60 @@ SELECT
 FROM atleta
 WHERE atleta.posicao = 'GOLEIRO';
 
-# Mostre a data da partida, o nome do campeonato e o nome do técnico que comandou o time naquele jogo em específico
+# data da partida, o nome do campeonato e o nome do técnico que comandou o time naquele jogo em específico
 SELECT
-    partida.data_hora AS 'Data do Jogo',
+    partidas.data_hora AS 'Data do Jogo',
     competicao.nome_camp AS 'Nome do Campeonato',
     tecnico.nome AS 'Técnico no comando do time'
-FROM partida
-JOIN competicao ON partida.c_id_competicao = competicao.id_competicao,
-JOIN tecnico ON partida.t_id_tecnico = tecnico.id_tecnico
-WHERE partida.c_id_competicao = 1,
-WHERE partida.t_id_tecnico = 1;
+FROM partidas
+JOIN competicao ON partidas.c_id_competicao = competicao.id_competicao
+JOIN tecnico ON partidas.t_id_tecnico = tecnico.id_tecnico;
+
+# nome e a posição de todos os atletas cujo nome comece com a letra 'E'.
+SELECT
+    atleta.nome AS 'Nome do Jogador',
+    atleta.posicao AS 'Posição'
+FROM atleta
+WHERE atleta.nome LIKE 'E%';
+
+# nome do jogador, a posição dele e a quantidade de cartões amarelos de todos os atletas que levaram pelo menos 1 cartão amarelo em qualquer jogo.
+SELECT
+    atleta.nome AS 'Jogador',
+    atleta.posicao AS 'Posição',
+    participacao.cartao_am AS 'Cartão Amarelo'
+FROM participacao
+INNER JOIN atleta ON participacao.id_partida = atleta.id_atleta
+WHERE participacao.cartao_am >= 1 
+
+# nome de todos os atacantes cadastrados que jogaram na partida número 1, mas que não fizeram nenhum gol
+SELECT
+    atleta.nome AS 'Nome do Jogador',
+    atleta.posicao AS 'Posição',
+    participacao.gols_marcados AS 'Gols Marcados'
+FROM participacao
+INNER JOIN atleta ON participacao.id_atleta = atleta.id_atleta
+WHERE partidas.id_partida = 1
+AND atleta.posicao = 'ATACANTE'
+AND participacao.gols_marcados = 0;
+
+# nome de cada campeonato cadastrado e a quantidade de partidas que pertencem a ele.
+SELECT
+    competicao.nome_camp AS 'Nome do Campeonato',
+    COUNT (partidas.id_partida) AS 'Total de Partidas Disputadas'
+FROM competicao 
+LEFT JOIN partidas ON competicao.id_competicao = partidas.c_id_competicao
+GROUP BY competicao.nome_camp
+
+# estatísticas de todos os jogadores, trazendo junto o nome do jogador e o nome do adversário que ele enfrentou.
+SELECT
+    atleta.nome AS 'Jogador',
+    adversario.nome_clube AS 'Adversário',
+    partidas.data_hora AS 'Data do Jogo',
+    participacao.minutos AS 'Minutos Jogados',
+    participacao.gols_marcados AS 'Gols',
+    participacao.assistencias AS 'Assistências'
+FROM participacao
+INNER JOIN atleta ON participacao.id_atleta = atleta.id_atleta
+INNER JOIN partidas ON participacao.id_partida = partidas.id_partida
+INNER JOIN adversario ON partidas.a_id_adversario = adversario.id_adversario;
+
